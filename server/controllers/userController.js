0c0a83afd176cfe5b6c73b59a2a780ef20a60bc8
Abhishek Sharma <bluesparingabhishek@gmail.com>
2025-07-03 01:30:26 +0530

    feat: Update environment configuration for API base URL and enhance CORS settings; improve security for cookie handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -159,6 +159,8 @@ export const refreshToken = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "development",
+      secure: true,
+      sameSite: "None",
       maxAge: 60 * 60 * 1000, // 1 hour
     });
 
